691d2135a8842c34b236361295a545b09feddea1
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-08 14:43:24 +0600

    init mongodb

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
 const app = express();
 const port = process.env.PORT || 3333;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection string
+const uri = `mongodb+srv://${process.env.mongodb_user}:${process.env.mongodb_pass}@cluster0.zzj1wzu.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.log("MongoDB Error:", error);
+  }
+}
+run();
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
