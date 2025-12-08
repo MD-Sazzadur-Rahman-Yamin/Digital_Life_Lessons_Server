a36485ac73539f2a030fa61e8119f8f1beb7bf78
MD-Sazzadur-Rahman-Yamin <mailme.yamin@gmail.com>
2025-12-09 00:26:50 +0600

    varify payment api /payment/payment-success

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
     const db = client.db("Digital_Life_Lessons_DB");
     //collections
     const users_coll = db.collection("users");
+    const payments_coll = db.collection("payments");
 
     //users APIs
     app.post("/users/sync", async (req, res) => {
@@ -75,8 +76,8 @@ async function run() {
             metadata: paymentInfo.metadata,
             customer_email: paymentInfo.customer_email,
             mode: "payment",
-            success_url: `${process.env.client_domain}?/upgrade-successful`,
-            cancel_url: `${process.env.client_domain}?/upgrade-failed`,
+            success_url: `${process.env.client_domain}/upgrade-successful?session_id={CHECKOUT_SESSION_ID}`,
+            cancel_url: `${process.env.client_domain}/upgrade-failed`,
           });
           res.send({ url: session.url });
         } catch (error) {
@@ -84,6 +85,55 @@ async function run() {
         }
       }
     );
+
+    app.patch("/payments/payment-success", async (req, res) => {
+      try {
+        const sessionId = req.query.session_id;
+        console.log(sessionId);
+        const session = await stripe.checkout.sessions.retrieve(sessionId);
+        console.log(session);
+
+        //stop duplicate entry
+        const query = { transactionId: session.payment_intent };
+        const paymentExist = await payments_coll.findOne(query);
+        if (paymentExist) {
+          return res.send(paymentExist);
+        }
+        //payment data
+        const payment = {
+          userEmail: session.customer_email,
+          stripeSessionId: sessionId,
+          currency: session.currency,
+          amount: session.amount_total / 100,
+          status: session.payment_status,
+          createdAt: new Date(),
+          transactionId: session.payment_intent,
+        };
+
+        //update user isPremium status
+        if (session.payment_status === "paid") {
+          const query = { email: session.customer_email };
+          const update = {
+            $set: {
+              isPremium: true,
+            },
+          };
+          const result = await users_coll.updateOne(query, update);
+
+          //add payment data in db
+          const paymentResult = await payments_coll.insertOne(payment);
+
+          res.send({
+            message: "Payment successful",
+            transactionId: payment.transactionId,
+            userModifiedCount: userResult.modifiedCount,
+            paymentInsertedId: paymentResult.insertedId,
+          });
+        }
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Connected to MongoDB!");
